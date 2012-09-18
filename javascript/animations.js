5eac0b5cdedde9537b1bdcb21fb9c185f91e3ab2
Ian Cooper <ian@americanwinery.com>
2012-09-18 10:41:32 -0700

    Finished main page layout and added some annimations.

--- javascript/animations.js
+++ javascript/animations.js
@@ -4,6 +4,12 @@ $(function() {
 	}).mouseout(function(){
 		$('ul li.imgBar',this).addClass("tan").removeClass("black");
 	});
+	
+	$(".upLink").mouseover(function(){
+		$(this).addClass("whiteText").removeClass("tanText");
+	}).mouseout(function(){
+		$(this).addClass("tanText").removeClass("whiteText");
+	});
 
 
 });
